4fc1ea37a6a75ae8df5f6537c674076b08dba32c
dkdldjswkd <dkdldjswkd@naver.com>
2020-08-18 15:58:19 +0900

    first commit

---
+++ FPSgame/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class PlayerControler : MonoBehaviour
+{
+    //스피드 변수
+    [SerializeField]
+    private float WalkSpeed;
+    [SerializeField]
+    private float RunSpeed;
+    private float ApplySpeed;
+
+    //상태 변수
+    private bool IsRun = false;
+
+    // 민감도 변수
+    [SerializeField]
+    private float LookSensitivity;
+
+    //카메라 한계
+    [SerializeField]
+    private float cameraRotationLimit = 90;
+    private float currentCameraRotationX = 0;
+
+    //필요한 컴포넌트
+    [SerializeField]
+    private Camera theCamera;
+    private Rigidbody myRigid;
+
+
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+        ApplySpeed = WalkSpeed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TryRun();
+        Move();
+        CameraRotation();
+        CharactorRotation();
+    }
+    private void Move()
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _moveDirX;
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * ApplySpeed;
+
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+    }
+
+    private void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    private void Running()
+    {
+        IsRun = true;
+        ApplySpeed = RunSpeed;
+    }
+    private void RunningCancel()
+    {
+        IsRun = false;
+        ApplySpeed = WalkSpeed;
+    }
+
+    private void CameraRotation()
+    {
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotationX = _xRotation * LookSensitivity;
+        currentCameraRotationX += _cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3 (-currentCameraRotationX, 0, 0);
+    }
+
+    private void CharactorRotation()
+    {
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * LookSensitivity;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
+        Debug.Log(myRigid.rotation);
+        Debug.Log(myRigid.rotation.eulerAngles);
+    }
+
+
+}
