58303e22b80ca10c673d40566e8c07056f4f224f
dkdldjswkd <dkdldjswkd@naver.com>
2020-08-26 21:34:02 +0900

    title scene

---
+++ FPSgame/Assets/Scripts/Title.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public string sceneName = "GameStage";
+    // Start is called before the first frame update
+    public void ClickStart()
+    {
+        Debug.Log("로딩");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ClickLoad()
+    {
+        Debug.Log("로드");
+    }
+
+    public void ClickExit()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+    }
+}
