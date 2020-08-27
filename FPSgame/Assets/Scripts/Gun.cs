0bc5e12e61edfaca1356a3a84a6052a673ceafce
dkdldjswkd <dkdldjswkd@naver.com>
2020-08-27 22:59:28 +0900

    gun 스크립트 코딩중

---
+++ FPSgame/Assets/Scripts/Gun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public string GunName; // 총 이름
+    public float range; // 사정거리
+    public float accuracy; // 정확도
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
