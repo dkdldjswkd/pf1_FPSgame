1cabf729202cdd78a4774027aa8bb4f3708b2956
dkdldjswkd <dkdldjswkd@naver.com>
2020-08-29 00:07:34 +0900

    총기섬광 구현

--- FPSgame/Assets/Scripts/Gun.cs
+++ FPSgame/Assets/Scripts/Gun.cs
@@ -7,15 +7,23 @@ public class Gun : MonoBehaviour
     public string GunName; // 총 이름
     public float range; // 사정거리
     public float accuracy; // 정확도
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float fireRate;//연사속도
+    public float reloadTime; // 재장전 속도
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int damage; // 총의 데미지
+
+    public int reloadBulletCount; // 총알 재장전 개수
+    public int currentBulletCount;//현재 탄알집에 남아있는 총알 개수
+    public int maxBulletCount; // 최대 소유 가능 개수
+    public int carryBulletCount; // 현재 소유하고 있는 총알 개수
+
+    public float retroActionForce; // 반동 세기
+    public float retroActionFineSightForce; // 정조준 시의 반동 세기
+
+    public Vector3 fineSightOriginPos;
+    public Animator anim;
+    public ParticleSystem muzzleFlash; //총구 섬광
+
+    public AudioClip Fire_Sound;
+
 }
